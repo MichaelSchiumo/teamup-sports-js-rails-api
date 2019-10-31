c874c119ecdd8e857d15a63a6db295303176f07c
Michael Schiumo <mschiumo18@gmail.com>
2019-10-31 17:52:20 -0400

    added initialize efforts

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,5 +1,9 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :team_id, :name, :position, :number, :captain, :hometown
 
+  def player(player_object)
+    @player = player_object
+  end
+
   belongs_to :team
-end  
+end
--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,5 +1,9 @@
 class TeamSerializer < ActiveModel::Serializer
   attributes :id, :name, :color, :rank
 
+  def initialize(team_object)
+    @team = team_object
+  end
+
   has_many :players
 end
