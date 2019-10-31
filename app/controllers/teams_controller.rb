fdab183131fc94aa956e974fc3228091cabde8e4
Michael Schiumo <mschiumo18@gmail.com>
2019-10-31 15:21:34 -0400

    added before action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,5 @@
 class TeamsController < ApplicationController
+  before_action :set_team, only: [:show, :update, :destroy]
 
   def index
     @teams = Team.all
@@ -28,38 +29,10 @@ class TeamsController < ApplicationController
 
   def team_params
     params.require(:team).permit(:name, :color, :rank)
-  end  
+  end
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
 
 end
-
-
-
-#   # POST /todos
-#   def create
-#     @todo = Todo.create!(todo_params)
-#     json_response(@todo, :created)
-#   end
-#
-#   # PUT /todos/:id
-#   def update
-#     @todo.update(todo_params)
-#     head :no_content
-#   end
-#
-#   # DELETE /todos/:id
-#   def destroy
-#     @todo.destroy
-#     head :no_content
-#   end
-#
-#   private
-#
-#   def todo_params
-#     # whitelist params
-#     params.permit(:title, :created_by)
-#   end
-#
-#   def set_todo
-#     @todo = Todo.find(params[:id])
-#   end
-# end
