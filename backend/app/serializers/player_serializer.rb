3de1e9f8cf917e813df133ec7ba915d68b79c9b1
Michael Schiumo <mschiumo18@gmail.com>
2019-10-31 19:31:28 -0400

    need to commit

---
+++ backend/app/serializers/player_serializer.rb
@@ -0,0 +1,17 @@
+class PlayerSerializer < ActiveModel::Serializer
+    include FastJsonapi::ObjectSerializer
+
+  attributes :team_id, :name, :position, :number, :captain, :hometown
+
+  def player(player_object)
+    @player = player_object
+  end
+
+  def to_serialized_json
+    @player.to_json(:include => {
+      :only => [:team_id, :name, :position, :number, :captain, :hometown]
+      })
+  end
+
+  belongs_to :team
+end
---
+++ backend/app/serializers/team_serializer.rb
@@ -0,0 +1,17 @@
+class TeamSerializer < ActiveModel::Serializer
+  include FastJsonapi::ObjectSerializer
+
+  attributes :id, :name, :color, :rank
+
+  def initialize(team_object)
+    @team = team_object
+  end
+
+  def to_serialized_json
+    @team.to_json(:include => {
+      :only => [:name, :color, :rank]
+    })
+  end
+
+  has_many :players
+end
