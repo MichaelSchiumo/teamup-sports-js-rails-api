91fecdf180ff3ecf1a187758e404523b8fcd0fdd
Michael Schiumo <mschiumo18@gmail.com>
2019-10-31 15:35:05 -0400

    added destroy, player params, and set player before action

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,5 @@
 class PlayersController < ApplicationController
+  before_action :set_player, only: [:show, :update, :destroy]
 
   def index
     @players = Player.all
@@ -27,43 +28,9 @@ class PlayersController < ApplicationController
 
   def player_params
     params.require(:player).permit(:name, :position, :number, :captain, :hometown)
-  end      
+  end
+
+  def set_player
+    @player = Player.find(params[:id])
+  end
 end
-
-
-
-# before_action :set_team, only: [:show, :update, :destroy]
-#
-# def index
-#   @teams = Team.all
-#   json_response(@teams)
-# end
-#
-# def create
-#   @team = Team.create!(team_params)
-#   json_response(@team, :created)
-# end
-#
-# def show
-#   json_reponse(@team)
-# end
-#
-# def update
-#   @team.update(team_params)
-#   head :no_content
-# end
-#
-# def destroy
-#   @team.destroy
-#   head :no_content
-# end
-#
-# private
-#
-# def team_params
-#   params.require(:team).permit(:name, :color, :rank)
-# end
-#
-# def set_team
-#   @team = Team.find(params[:id])
-# end
