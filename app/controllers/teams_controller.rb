18738b4dec30f165b30675d0414c562a3cfdcccd
Michael Schiumo <mschiumo18@gmail.com>
2019-10-31 15:16:03 -0400

    added show and update actions

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,49 +8,47 @@ class TeamsController < ApplicationController
   def create
     @team = Team.create!(team_params)
     json_response(@team, :created)
-  end  
-end
-
-class TodosController < ApplicationController
-  before_action :set_todo, only: [:show, :update, :destroy]
-
-  # GET /todos
-  def index
-    @todos = Todo.all
-    json_response(@todos)
   end
 
-  # POST /todos
-  def create
-    @todo = Todo.create!(todo_params)
-    json_response(@todo, :created)
-  end
-
-  # GET /todos/:id
   def show
-    json_response(@todo)
+    json_reponse(@team)
   end
 
-  # PUT /todos/:id
   def update
-    @todo.update(todo_params)
+    @team.update(team_params)
     head :no_content
-  end
+  end  
 
-  # DELETE /todos/:id
-  def destroy
-    @todo.destroy
-    head :no_content
-  end
-
-  private
-
-  def todo_params
-    # whitelist params
-    params.permit(:title, :created_by)
-  end
-
-  def set_todo
-    @todo = Todo.find(params[:id])
-  end
 end
+
+
+
+#   # POST /todos
+#   def create
+#     @todo = Todo.create!(todo_params)
+#     json_response(@todo, :created)
+#   end
+#
+#   # PUT /todos/:id
+#   def update
+#     @todo.update(todo_params)
+#     head :no_content
+#   end
+#
+#   # DELETE /todos/:id
+#   def destroy
+#     @todo.destroy
+#     head :no_content
+#   end
+#
+#   private
+#
+#   def todo_params
+#     # whitelist params
+#     params.permit(:title, :created_by)
+#   end
+#
+#   def set_todo
+#     @todo = Todo.find(params[:id])
+#   end
+# end
