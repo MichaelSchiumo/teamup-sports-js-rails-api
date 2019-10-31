b77ff9db954fc150b0b578d6e2c981cbab944ca5
Michael Schiumo <mschiumo18@gmail.com>
2019-10-31 16:02:56 -0400

    removed instance variables from entire controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,22 +7,22 @@ class TeamsController < ApplicationController
   end
 
   def create
-    @team = Team.create!(team_params)
-    json_response(@team, :created)
+    team = Team.create!(team_params)
+    json_response(team, :created)
   end
 
   def show
-    bird = Bird.find_by(id: params[:id])
-    render json: bird
+    team = Team.find_by(id: params[:id])
+    render json: team
   end
 
   def update
-    @team.update(team_params)
+    team.update(team_params)
     head :no_content
   end
 
   def destroy
-    @team.destroy
+    team.destroy
     head :no_content
   end
 
@@ -33,7 +33,7 @@ class TeamsController < ApplicationController
   end
 
   def set_team
-    @team = Team.find(params[:id])
+    team = Team.find(params[:id])
   end
 
 end
