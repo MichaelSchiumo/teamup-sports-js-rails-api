5e94df1d4e11287fa510c08cf8364f00a598cb83
Michael Schiumo <mschiumo18@gmail.com>
2019-11-01 09:42:33 -0400

    removed to serialized json method

--- backend/app/controllers/players_controller.rb
+++ backend/app/controllers/players_controller.rb
@@ -3,7 +3,7 @@ class PlayersController < ApplicationController
 
   def index
     players = Player.all
-    render json: PlayerSerializer.new(players).to_serialized_json
+    render json: PlayerSerializer.new(players)
   end
 
   def create
@@ -18,7 +18,7 @@ class PlayersController < ApplicationController
 
   def show
     player = Player.find_by(id: params[:id])
-    render json: PlayerSerializer.new(player).to_serialized_json
+    render json: PlayerSerializer.new(player)
   end
 
   def update
