c3f8d027268be658530a235c04187ad5d27bb0a7
Michael Schiumo <mschiumo18@gmail.com>
2019-11-01 09:41:35 -0400

    fixed syntax

--- backend/app/serializers/player_serializer.rb
+++ backend/app/serializers/player_serializer.rb
@@ -3,15 +3,15 @@ class PlayerSerializer < ActiveModel::Serializer
 
   attributes :team_id, :name, :position, :number, :captain, :hometown
 
-  def player(player_object)
-    @player = player_object
-  end
-
-  def to_serialized_json
-    @player.to_json(:include => {
-      :only => [:team_id, :name, :position, :number, :captain, :hometown]
-      })
-  end
+  # def player(player_object)
+  #   @player = player_object
+  # end
+  #
+  # def to_serialized_json
+  #   @player.to_json(:include => {
+  #     :only => [:team_id, :name, :position, :number, :captain, :hometown]
+  #     })
+  # end
 
   belongs_to :team
 end
