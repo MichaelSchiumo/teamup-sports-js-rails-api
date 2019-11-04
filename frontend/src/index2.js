f4376371c4674bfbecc318c04040646f0213fa13
Michael Schiumo <mschiumo18@gmail.com>
2019-11-04 17:22:48 -0500

    working on returning team objects from JSON

--- frontend/src/index2.js
+++ frontend/src/index2.js
@@ -2,7 +2,7 @@ const BASE_URL = "http://localhost:3000";
 const TEAMS_URL = `${BASE_URL}/teams`;
 const PlAYERS_URL = `${BASE_URL}/players`;
 
-let teams = [];
+
 
 document.addEventListener("DOMContentLoaded", function() {
   fetchTeams();
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
 function fetchTeams() {
   const teamsURL = "http://localhost:3000/teams";
 
-  return fetch('http://localhost:3000/teams')
+  return fetch(teamsURL)
       .then(response => response.json())
       //promise
       // .then(result => renderTeams(json))
@@ -22,8 +22,8 @@ function fetchTeams() {
       })
   };
 
-function renderTeams(teams) {
-  for(team in teams) {
+function renderTeams(json) {
+  for (let team in json) {
     const teamsContainer = document.getElementById("teams-container")
     const teamsDiv = document.createElement("div")
     const teamsSpan = document.createElement("span")
@@ -39,7 +39,8 @@ function renderTeams(teams) {
 
     teamName.innerHTML = team.name;
 
-    saveTeam(team)
+    teams.push(team)
+    // saveTeam(team)
   }
 }
 
@@ -48,13 +49,38 @@ const getTeams = async () => {
     renderTeams(teams)
 }
 
-const saveTeam = (team = null) => {
-  if (team != null) {
-      teams.push(team)
-  } else {
-      throw new Error("HALP ME")
-  }
-}
+// const saveTeam = (team = null) => {
+//   if (team != null) {
+//       teams.push(team)
+//   } else {
+//       throw new Error("HALP ME")
+//   }
+// }
+
+function createTeam() {
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let teamData = {
+      name: teamName,
+      color: teamColor,
+      rank: teamRank
+    }
+    return (
+      fetch('http://localhost:3000/teams', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(teamData)
+      })
+    );
+  })
+};
+
+
+
 
 
 const generateTeamForm = () => {
