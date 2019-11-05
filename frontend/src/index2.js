e869d5664c5b4678f22cb047a846e8ccad3a7fe7
Michael Schiumo <mschiumo18@gmail.com>
2019-11-04 19:08:49 -0500

    playing with fetch and render function

--- frontend/src/index2.js
+++ frontend/src/index2.js
@@ -18,31 +18,76 @@ function fetchTeams() {
       // .then(result => renderTeams(json))
       .then(json => {
         renderTeams(json);
+        console.log(json)
         //here is where we fetch players
       })
   };
 
 function renderTeams(json) {
-  for (let team in json) {
-    const teamsContainer = document.getElementById("teams-container")
-    const teamsDiv = document.createElement("div")
-    const teamsSpan = document.createElement("span")
-    const teamName = document.createElement("p")
+  json.forEach(team => {
+    const teamsContainer = document.getElementById("div.teams-container");
+    const teamsDiv = document.createElement("div");
+    const teamNameEl = document.createElement("h2");
 
-
-    teamsContainer.innerHTML = 'My Teams Here'
     teamsDiv.className = "team";
-    teamName.className = "team-name"
+    teamName.className = "team-name";
 
-    teamsContainer.appendChild(teamsDiv);
-    teamsContainer.appendChild(teamName);
+    console.log(team)
 
-    teamName.innerHTML = team.name;
 
-    teams.push(team)
-    // saveTeam(team)
-  }
+  })
 }
+
+function renderToys(toys){
+  toys.forEach(toy => {
+    let cardEl = document.createElement("div");
+    let toyNameEl = document.createElement("h2");
+    let imgEl = document.createElement("img");
+    let toyLikesEl = document.createElement("p");
+    let likeBtnEl = document.createElement("button");
+
+    cardEl.className = "card";
+
+    imgEl.src = toy.image;
+
+    imgEl.className = "toy-avatar";
+
+    likeBtnEl.className = "like-btn";
+
+    toyLikesEl.className = `${toy.name}-likes-container`;
+
+
+    cardEl.appendChild(toyNameEl);
+    cardEl.appendChild(imgEl);
+    cardEl.appendChild(toyLikesEl);
+    cardEl.appendChild(likeBtnEl);
+
+    toyCollection.appendChild(cardEl);
+
+    toyNameEl.innerText = toy.name;
+
+    toyLikesEl.innerText = toy.likes;
+
+    likeBtnEl.innerText = "Like This Toy!";
+
+    likeBtnEl.addEventListener("click",function() {increaseToyLikes(toy)});
+
+
+  })
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const getTeams = async () => {
     teams = await fetchTeams()
@@ -57,29 +102,27 @@ const getTeams = async () => {
 //   }
 // }
 
-function createTeam() {
-  form.addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    let teamData = {
-      name: teamName,
-      color: teamColor,
-      rank: teamRank
-    }
-    return (
-      fetch('http://localhost:3000/teams', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify(teamData)
-      })
-    );
-  })
-};
-
-
+// function createTeam() {
+//   form.addEventListener("submit", function(e) {
+//     e.preventDefault();
+//
+//     let teamData = {
+//       name: teamName,
+//       color: teamColor,
+//       rank: teamRank
+//     }
+//     return (
+//       fetch('http://localhost:3000/teams', {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           "Accept": "application/json"
+//         },
+//         body: JSON.stringify(teamData)
+//       })
+//     );
+//   })
+// };
 
 
 
--- frontend/src/teams.js
+++ frontend/src/teams.js
@@ -1,4 +1,4 @@
-class Teams {
+class Team {
   constructor(id, name, color, rank) {
     this.id = id;
     this.teamName = name;
@@ -6,24 +6,10 @@ class Teams {
     this.teamRank = rank;
   }
 
-static buildTeamObjects(teams) {
-  return teams.map((team) => {
-    return new Team(team.id, team.name, team.color, team.rank)
-  })
-}
-
-  renderTeam() {
-    const teamDiv = document.createElement('div')
-    team.className = 'team-card'
-    teamDiv.dataset.id = this.id
-    // teamDiv.dataset.userID = this.user.id
-    teamText.className = 'text'
-    teamDiv.appendChild(teamText)
-    console.log(this)
-
-    teamText.innerHTML = `${this.name}`
-
-    return teamDiv
+  static buildTeamObjects(teams) {
+    return teams.map((team) => {
+      return new Team(team.id, team.name, team.color, team.rank)
+    })
   }
 
 }
