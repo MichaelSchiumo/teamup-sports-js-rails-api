c0899977cc946fb9c9b6fc8870f84f2b402b3094
Michael Schiumo <mschiumo18@gmail.com>
2019-11-04 19:57:59 -0500

    fixed fetch error

--- frontend/src/index2.js
+++ frontend/src/index2.js
@@ -2,7 +2,7 @@ const BASE_URL = "http://localhost:3000";
 const TEAMS_URL = `${BASE_URL}/teams`;
 const PlAYERS_URL = `${BASE_URL}/players`;
 
-
+const teams = [];
 
 document.addEventListener("DOMContentLoaded", function() {
   fetchTeams();
@@ -10,119 +10,71 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 function fetchTeams() {
-  const teamsURL = "http://localhost:3000/teams";
-
-  return fetch(teamsURL)
+  return fetch(TEAMS_URL)
       .then(response => response.json())
       //promise
       // .then(result => renderTeams(json))
       .then(json => {
-        renderTeams(json);
-        console.log(json)
+        renderTeams(json.data);
+        console.log(json);
         //here is where we fetch players
       })
   };
 
-function renderTeams(json) {
-  json.forEach(team => {
-    const teamsContainer = document.getElementById("div.teams-container");
-    const teamsDiv = document.createElement("div");
-    const teamNameEl = document.createElement("h2");
-
-    teamsDiv.className = "team";
-    teamName.className = "team-name";
-
+function renderTeams(teamsArray) {
+  teamsArray.forEach(team => {
     console.log(team)
-
-
   })
 }
 
-function renderToys(toys){
-  toys.forEach(toy => {
-    let cardEl = document.createElement("div");
-    let toyNameEl = document.createElement("h2");
-    let imgEl = document.createElement("img");
-    let toyLikesEl = document.createElement("p");
-    let likeBtnEl = document.createElement("button");
 
-    cardEl.className = "card";
+function saveTeam() {
+  //post request with team data to store in backend
 
-    imgEl.src = toy.image;
-
-    imgEl.className = "toy-avatar";
-
-    likeBtnEl.className = "like-btn";
-
-    toyLikesEl.className = `${toy.name}-likes-container`;
-
-
-    cardEl.appendChild(toyNameEl);
-    cardEl.appendChild(imgEl);
-    cardEl.appendChild(toyLikesEl);
-    cardEl.appendChild(likeBtnEl);
-
-    toyCollection.appendChild(cardEl);
-
-    toyNameEl.innerText = toy.name;
-
-    toyLikesEl.innerText = toy.likes;
-
-    likeBtnEl.innerText = "Like This Toy!";
-
-    likeBtnEl.addEventListener("click",function() {increaseToyLikes(toy)});
-
-
-  })
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const getTeams = async () => {
-    teams = await fetchTeams()
-    renderTeams(teams)
 }
 
-// const saveTeam = (team = null) => {
-//   if (team != null) {
-//       teams.push(team)
-//   } else {
-//       throw new Error("HALP ME")
-//   }
+// return (
+//     fetch("http://localhost:7000/toys", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//       },
+//       body: JSON.stringify(toyData)
+//     })
+//   );
+
+//
+//
+//
+// const getTeams = async () => {
+//     teams = await fetchTeams()
+//     renderTeams(teams)
 // }
 
-// function createTeam() {
-//   form.addEventListener("submit", function(e) {
-//     e.preventDefault();
-//
-//     let teamData = {
-//       name: teamName,
-//       color: teamColor,
-//       rank: teamRank
-//     }
-//     return (
-//       fetch('http://localhost:3000/teams', {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//           "Accept": "application/json"
-//         },
-//         body: JSON.stringify(teamData)
-//       })
-//     );
-//   })
-// };
+
+
+function createTeam() {
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let teamData = {
+      name: teamName,
+      color: teamColor,
+      rank: teamRank
+    }
+    return (
+      fetch('http://localhost:3000/teams', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(teamData)
+      })
+    );
+  })
+};
 
 
 
