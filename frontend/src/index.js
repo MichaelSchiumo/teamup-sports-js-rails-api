36c0947e9f6f8a276494a0a4632a0689a736d0a7
Michael Schiumo <mschiumo18@gmail.com>
2019-11-05 16:04:22 -0500

    fixed fetch and render functions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,14 @@ document.addEventListener("DOMContentLoaded", function() {
   generateTeamForm();
   fetchTeams();
   teamsContainer.appendChild(generateTeamForm());
+  document.querySelector('#create-team').addEventListener("submit", function(e) {
+    e.preventDefault();
+    saveTeam(e.target.elements);
+    document.querySelector('#team-wrapper').innerHTML = '';
+    // teamsContainer.innerHTML = '';
+    fetchTeams();
+
+  })
 });
 
 
@@ -21,21 +29,30 @@ function fetchTeams() {
       })
   };
 
+//take our teamsArray, pass it to the buildTeamObjects function, and access the data as team.name, team.color etc.
+
 function renderTeams(teamsArray) {
+  let teamWrapper = document.createElement("div")
+  teamWrapper.id = 'team-wrapper'
   teamsArray.forEach(team => {
+
+    // let newTeam = new Team(team)
 
     const h2 = document.createElement("h2");
     h2.innerHTML = `${team.attributes.name}`
-    teamsContainer.appendChild(h2)
+    teamsContainer.appendChild(teamWrapper)
+    teamWrapper.appendChild(h2)
 
     const teamColorEl = document.createElement("h4")
-    teamColorEl.innerHTML = `Color: ${team.attributes.color.charAt(0).toUpperCase() + team.attributes.color.slice(1)}`
+    teamColorEl.innerHTML = `Color: ${team.attributes.color.charAt(0).toUpperCase() +
+      team.attributes.color.slice(1)}`
     h2.appendChild(teamColorEl)
 
     const teamRank = document.createElement("h4")
     teamRank.innerHTML = `Rank: ${team.attributes.rank}`
     h2.appendChild(teamRank)
     //edit button
+    // teamWrapper.appendChild(h2)
 
   })
 }
@@ -59,38 +76,25 @@ function renderTeams(teamsArray) {
 // }
 
 
-
-
-
-function renderTrainers(json) {
-  const main = document.getElementsByTagName("main")[0];
-  json.forEach(trainer => {
-    const div = document.createElement("div");
-    div.setAttribute("class", "card");
-    div.setAttribute("data-id", `${trainer.id}`);
-    main.appendChild(div);
-    const p = document.createElement("p");
-    p.textContent = `${trainer.name}`;
-    div.appendChild(p);
-    const addButton = document.createElement("button");
-    addButton.setAttribute("data-trainer-id", `${trainer.id}`);
-    addButton.textContent = "Add Pokemon";
-    addButton.addEventListener("click", onClickAdd);
-    div.appendChild(addButton);
-    const ul = document.createElement("ul");
-    div.appendChild(ul);
-  });
-}
-
-
 // const getTeams = async () => {
 //   teams = await fetchTeams()
 //   renderTeams()
 // }
 
 
-function saveTeam() {
+//what do we do after saveTeam?
+
+function saveTeam(team) {
   //post request with team data to store in backend
+  //assign team
+  //get each individual string
+
+  let teamData = {
+    name: team.name.value,
+    color: team.color.value,
+    rank: team.rank.value
+  }
+
   return (
         fetch("http://localhost:3000/teams", {
           method: "POST",
@@ -104,17 +108,14 @@ function saveTeam() {
 }
 
 
-
-
-
 const generateTeamForm = () => {
-    const form = document.createElement('teamForm')
+  //use css
+    const form = document.createElement('form')
     // const formInput = document.createElement('input')
     const formSubmit = document.createElement('button')
     formSubmit.textContent = "Create Team"
     //write loop to create multiple inputs
     //append inputs to form
-
     form.id = "create-team"
     formSubmit.id = "form-submit"
 
@@ -124,34 +125,16 @@ const generateTeamForm = () => {
     form.appendChild(nameInput)
 
     let colorInput = document.createElement("input")
-    colorInput.setAttribute('color', 'color')
+    colorInput.setAttribute('name', 'color')
     colorInput.placeholder = "Team Color"
     form.appendChild(colorInput)
 
     let rankInput = document.createElement("input")
-    rankInput.setAttribute('rank', 'rank')
+    rankInput.setAttribute('name', 'rank')
     rankInput.placeholder = "Team Rank"
     form.appendChild(rankInput)
 
-    // formInput.id = "team-form-text"
-    // formInput.name = "formInput"
-
-
-
-    // form.appendChild(formInput)
     form.appendChild(formSubmit)
 
     return form
-}
-
-
-
-function newTeamButton() {
-    let button = document.createElement('button')
-    button.textContent = 'Create a Team'
-    button.addEventListener('click', function() {
-        teamsContainer.innerHTML = ''
-        teamsContainer.appendChild(generateTeamForm())
-    })
-    return button
 }
