512a7a2d2c89cd91430d4a474aaa832a1a591b06
Michael Schiumo <mschiumo18@gmail.com>
2019-11-07 18:04:33 -0500

    just in case

--- backend/app/models/player.rb
+++ backend/app/models/player.rb
@@ -1,3 +1,8 @@
 class Player < ApplicationRecord
   belongs_to :team
+
+  validates :name, presence: true
+  validates :hometown, presence: true
+  validates :position, presence: true
+  validates :number, presence: true
 end
